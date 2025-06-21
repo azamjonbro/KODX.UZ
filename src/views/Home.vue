1e9f8af441b9cf5c1384b4adcebca8541a02bd7e
azamjonbro <azamjondasturchi11@gmail.com>
2025-06-22 01:55:57 +0500

    change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
        <div class="center-intro-icon">
     <div class="bracket">{</div>
     <div style="display: flex;">
-      <div class="typing">KODX<span class="dotuz">.UZ</span></div>
+      <div class="typing">KODX.UZ</div>
     </div>
     <div class="bracket">}</div>
   </div>
