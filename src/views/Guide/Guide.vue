1b715643b4ba504c640d2381488db87bab0bb33c
azamjonbro <azamjondasturchi11@gmail.com>
2025-06-23 01:55:55 +0500

    added

---
+++ src/views/Guide/Guide.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, watch } from 'vue'
+import MarkdownIt from 'markdown-it'
+
+const md = new MarkdownIt()
+const markdownText = ref('# Salom!\nBu **markdown** matni.')
+const renderedHtml = ref(md.render(markdownText.value))
+
+watch(markdownText, (newVal) => {
+ renderedHtml.value = md.render(newVal)
+})
+</script>
+
+<template>
+ <textarea v-model="markdownText" rows="8" cols="50"></textarea>
+ <div v-html="renderedHtml"></div>
+</template>
